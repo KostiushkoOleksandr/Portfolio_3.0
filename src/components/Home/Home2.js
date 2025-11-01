ba7a3e8563d16a75e696f127c20858f25ed2d2bd
Oleksandr Kostiushko <oleksander.kostiushko@gmail.com>
2025-11-01 19:39:50 +0200

    main

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/portfolio.jpg";
+import myImg from "../../Assets/alexkost.jpg";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
