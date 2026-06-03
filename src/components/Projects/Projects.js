ac436ad68eb9551500fb2934e9405b5cf3fe9479
Oleksandr Kostiushko <oleksandr.kostiushko@oa.edu.ua>
2026-06-03 18:12:08 +0300

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -41,16 +41,16 @@ function Projects() {
             />
           </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-                imgPath={poetry}
-                isBlog={false}
-                title="AlexKost-Poetry"
-                description="I began writing poems, inspired by life events and emotions that cannot always be expressed in simple words."
-                ghLink="https://github.com/KostiushkoOleksandr/my-poetry-app"
-                demoLink="https://alexkost-poetry.vercel.app/"
-            />
-          </Col>
+          // <Col md={4} className="project-card">
+          //   <ProjectCard
+          //       imgPath={poetry}
+          //       isBlog={false}
+          //       title="AlexKost-Poetry"
+          //       description="I began writing poems, inspired by life events and emotions that cannot always be expressed in simple words."
+          //       ghLink="https://github.com/KostiushkoOleksandr/my-poetry-app"
+          //       demoLink="https://alexkost-poetry.vercel.app/"
+          //   />
+          // </Col>
         </Row>
       </Container>
     </Container>
